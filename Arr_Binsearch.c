b438c697a57d5cff49f5b79660f1e611587a6780
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-04-02 09:24:23 +0530

    Arr_Binsearch.c

--- Arr_Binsearch.c
+++ Arr_Binsearch.c
@@ -1,45 +1,50 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX 10
+
+void binary(int *a);
+
 int main()
 {
-    int arr[10],i;
-    printf("\n Enter %d Elements :",MAX);
-    for(i=0;i<MAX;i++)
+    int arr[MAX], i;
+    printf("\n Enter %d Elements: ", MAX);
+    for (i = 0; i < MAX; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
     binary(arr);
+    return 0;
 }
+
 void binary(int *a)
 {
-    int num,low,high,mid,flag=0;
+    int num, low, high, mid, flag = 0;
     printf("\n Enter the number you want to search: ");
-    scanf("%d",&num);
-    low=0;
-    high=MAX-1;
-    while(low<=high)
-      {
-          mid=(low+high) / 2;
-        if(a[mid]==num)
+    scanf("%d", &num);
+    low = 0;
+    high = MAX - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (a[mid] == num)
         {
             printf("\n Search is Successful ");
-            flag=1;
-            printf("\n Element is present at position %d",mid);
+            flag = 1;
+            printf("\n Element is present at position %d", mid);
             break;
         }
         else
         {
-            if(a[mid]<num)
+            if (a[mid] < num)
             {
-                low=mid+1;
+                low = mid + 1;
             }
             else
             {
-                high=mid-1;
+                high = mid - 1;
             }
         }
     }
-    if(flag==0)
+    if (flag == 0)
     {
         printf("\n Search is unsuccessful: Element is not present");
     }
